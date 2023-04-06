2fa4b1379205baf941613b52624e6a7fc211057a
Hsuan19971005 <g19971005@gmail.com>
2023-04-06 23:56:40 +0800

    [WIP]sign up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :password, presence: true, confirmation: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/}
 end
