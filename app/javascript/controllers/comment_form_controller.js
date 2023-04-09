c7dca22433fd5742e1302cd3910fc6cf1e40e27b
Hsuan19971005 <g19971005@gmail.com>
2023-04-10 00:26:07 +0800

    STIMULUS disable submit button while text_area is blank

---
+++ app/javascript/controllers/comment_form_controller.js
@@ -0,0 +1,13 @@
+import {Controller} from "stimulus";
+
+export default class extends Controller {
+  static targets = ["button"];
+  input(e) {
+    const content = e.target.value.trim();
+    if (content != "") {
+      this.buttonTarget.removeAttribute("disabled");
+    } else {
+      this.buttonTarget.setAttribute("disabled", "");
+    }
+  }
+}
