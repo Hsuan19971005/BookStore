26c095056d19d6ac54cb00ed3d70ff43a4c5771c
Hsuan19971005 <g19971005@gmail.com>
2023-04-10 21:59:46 +0800

    edit comments, delete comments by HotWire

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,39 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_book, only: [:create]
   before_action :find_comment, only: [:destroy, :edit, :update]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.book_id = @book.id
     @comment.user_id = current_user.id
-    
+
     if @comment.save
       # redirect_to book_path(@book.id), notice: "New comment!"
       # create.js.erb
     else
       @comments = @book.comments.where(user_id: current_user.id).order(created_at: :desc)
-      render 'books/show'
+      render "books/show"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to book_path(@comment.book_id), notice: "Update comment successfully!"
+    else
+      render :edit
     end
   end
 
   def destroy
     @comment.destroy
-    redirect_to book_path(@comment.book_id), notice: "New comment!"
+    # redirect_to book_path(@comment.book_id), notice: "New comment!"
   end
 
-
   private
+
   def comment_params
     params.require(:comment).permit(:content)
   end
